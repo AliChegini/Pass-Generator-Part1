94e3984fef63f3b52369b71785ce543ab468065a
Ali Chegini <ali.c.engineer@gmail.com>
2018-01-23 07:23:46 +0100

    improved model and added initializers

--- Pass-Generator/Model.swift
+++ Pass-Generator/Model.swift
@@ -6,9 +6,6 @@
 //  Copyright © 2018 Ali C. All rights reserved.
 //
 
-import Foundation
-
-
 enum AreaAccess {
     case amusementAreas
     case kitchenAreas
@@ -29,43 +26,44 @@ protocol DiscountAccess {
 }
 
 
-protocol RequiredInfo {
-    var dateOfBirth: Data { get set }
-}
-
-
 protocol AreaAndRideAccess {
     var areaAccess: [AreaAccess] { get }
     var rideAccess: [RideAccess] { get }
-    
 }
 
 
 struct ClassicGuest: AreaAndRideAccess {
     var areaAccess: [AreaAccess] = [.amusementAreas]
     var rideAccess: [RideAccess] = [.accessAllRides]
-    
 }
 
 
 struct VIPGuest: AreaAndRideAccess, DiscountAccess {
-    
     var areaAccess: [AreaAccess] = [.amusementAreas]
     var rideAccess: [RideAccess] = [.accessAllRides, .skipAllRides]
     
     var discountOnFood: Int = 10
     var discountOnMerchandise: Int = 20
-
 }
 
 
 struct FreeChildGuest: AreaAndRideAccess {
     var areaAccess: [AreaAccess] = [.amusementAreas]
     var rideAccess: [RideAccess] = [.accessAllRides]
-    
 }
 
-class Staff {
+// Protocol for Staff info
+protocol StaffInfo {
+    var firstName: String { get }
+    var lastName: String { get }
+    var streetAddress: String { get }
+    var city: String { get }
+    var state: String { get }
+    var zipCode: String { get }
+}
+
+// Structs to represent different types of employees
+struct EmployeeFoodService: StaffInfo, AreaAndRideAccess, DiscountAccess {
     var firstName: String
     var lastName: String
     var streetAddress: String
@@ -73,7 +71,16 @@ class Staff {
     var state: String
     var zipCode: String
     
-    init(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String) {
+    var areaAccess: [AreaAccess] = [.amusementAreas, .kitchenAreas]
+    var rideAccess: [RideAccess] = [.accessAllRides]
+    
+    var discountOnFood: Int = 15
+    var discountOnMerchandise: Int = 25
+    
+    init?(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String) {
+        if firstName.isEmpty || lastName.isEmpty || streetAddress.isEmpty || city.isEmpty || state.isEmpty || zipCode.isEmpty {
+            return nil
+        }
         self.firstName = firstName
         self.lastName = lastName
         self.streetAddress = streetAddress
@@ -81,8 +88,89 @@ class Staff {
         self.state = state
         self.zipCode = zipCode
     }
-    
 }
 
 
+struct EmployeeRideService : StaffInfo, AreaAndRideAccess, DiscountAccess {
+    var firstName: String
+    var lastName: String
+    var streetAddress: String
+    var city: String
+    var state: String
+    var zipCode: String
+    
+    var areaAccess: [AreaAccess] = [.amusementAreas, .rideControlAreas]
+    var rideAccess: [RideAccess] = [.accessAllRides]
+    
+    var discountOnFood: Int = 15
+    var discountOnMerchandise: Int = 25
+    
+     // Failable initializer
+    init?(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String) {
+        if firstName.isEmpty || lastName.isEmpty || streetAddress.isEmpty || city.isEmpty || state.isEmpty || zipCode.isEmpty {
+            return nil
+        }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.streetAddress = streetAddress
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+    }
+}
 
+
+struct EmployeeMaintenance: StaffInfo, AreaAndRideAccess, DiscountAccess {
+    var firstName: String
+    var lastName: String
+    var streetAddress: String
+    var city: String
+    var state: String
+    var zipCode: String
+    
+    var areaAccess: [AreaAccess] = [.amusementAreas, .kitchenAreas, .rideControlAreas, .maintenanceAreas]
+    var rideAccess: [RideAccess] = [.accessAllRides]
+    
+    var discountOnFood: Int = 15
+    var discountOnMerchandise: Int = 25
+    
+    init?(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String) {
+        if firstName.isEmpty || lastName.isEmpty || streetAddress.isEmpty || city.isEmpty || state.isEmpty || zipCode.isEmpty {
+            return nil
+        }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.streetAddress = streetAddress
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+    }
+}
+
+
+struct Manager: StaffInfo, AreaAndRideAccess, DiscountAccess {
+    var firstName: String
+    var lastName: String
+    var streetAddress: String
+    var city: String
+    var state: String
+    var zipCode: String
+    
+    var areaAccess: [AreaAccess] = [.amusementAreas, .kitchenAreas, .rideControlAreas, .maintenanceAreas, .officeAreas]
+    var rideAccess: [RideAccess] = [.accessAllRides]
+    
+    var discountOnFood: Int = 25
+    var discountOnMerchandise: Int = 25
+    
+    init?(firstName: String, lastName: String, streetAddress: String, city: String, state: String, zipCode: String) {
+        if firstName.isEmpty || lastName.isEmpty || streetAddress.isEmpty || city.isEmpty || state.isEmpty || zipCode.isEmpty {
+            return nil
+        }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.streetAddress = streetAddress
+        self.city = city
+        self.state = state
+        self.zipCode = zipCode
+    }
+}
